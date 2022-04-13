bbb0bc2173c79d1756664365e7f79f6e35c4f629
tymicruz <tymicruz@gmail.com>
2022-04-13 19:29:05 -0400

    Change how locked blocks are displayed (inline style). Remove unused css.

--- src/components/Block.js
+++ src/components/Block.js
@@ -2,8 +2,13 @@ import { useState } from "react"
 
 const Block = (props) => {
 
+    const styles = {
+        backgroundColor: props.isLocked ? "#90EE90" : "",
+        color: props.isLocked ? "white" : ""
+    }
+
     return (
-        <div className={`${"block"} ${props.isLocked ? "locked" : ""}`} onClick={() => props.handleToggle(props.id)}>
+        <div className="block" style={styles} onClick={() => props.handleToggle(props.id)}>
             <p>{props.number}</p>
         </div>
     )
